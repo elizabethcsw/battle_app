269996e46fe72afafbb99f4338228e8ac8db2ac2
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-08-17 16:50:00 +0100

    Attacking Player 2 (without feature test)

--- app.rb
+++ app.rb
@@ -7,7 +7,6 @@ class Battle < Sinatra::Base
   enable :sessions
 
   get '/' do
-    # socket.puts 'Testing infrastructure working!'
     erb :index
   end
 
@@ -16,7 +15,7 @@ class Battle < Sinatra::Base
     p session[:player1] = params[:name1]
     p session[:player2] = params[:name2]
     redirect '/play'
-    save_and_open_page
+    # save_and_open_page
   end
 
   get '/play' do
@@ -24,8 +23,23 @@ class Battle < Sinatra::Base
     @name2 = session[:player2]
     @player1_hp = 100
     @player2_hp = 100
+    session[:player1_hp] = @player1_hp
+    session[:player2_hp] = @player2_hp
     erb :play
   end
+
+  post "/play" do
+    p params
+    @name1 = session[:player1]
+    @name2 = session[:player2]
+    @player1_hp = session[:player1_hp]
+    @player2_hp = session[:player2_hp]
+    @player1_hp-=10
+    session[:player1_hp] = @player1_hp
+    session[:player2_hp] = @player2_hp
+    erb :play
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
